2566eff2c815c89fc3098803f4900a2695b074c4
mohammad hamzeie <m.hamzeie@sepasholding.com>
2023-11-06 17:09:32 +0330

    1402-08-15

---
+++ go/The Go Programming Language/examples/array/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// func main() {
+// 	a := [...]int{1,2}
+// 	fmt.Printf("%v", a)
+// }
+
+
+// func main() {
+// 	var a int
+// 	fmt.Println(&a)
+// }
+
+// func main() {
+// 	var p *int
+// 	fmt.Println(&p)
+// }
+
+func main() {
+	var p *int
+	fmt.Println(p)
+}
---
+++ go/The Go Programming Language/examples/dataType/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// func main() {
+// 	x := complex(1, 2)
+// 	fmt.Println(x)
+// }
+
+func main() {
+	s := "left foot"
+	t := s 
+	s += ", right foot" 
+	d := s + "man ezafe shodam"
+	fmt.Println(s)
+	fmt.Println(t)
+	fmt.Println(d)
+}
